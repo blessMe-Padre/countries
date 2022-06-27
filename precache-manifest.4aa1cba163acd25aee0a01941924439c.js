ad375a8baf90143cb219f12e1ed8f814ebaffa58
Сергей Субботин <zarodiny@yandex.ru>
2022-06-27 19:49:36 +1000

    Updates

---
+++ precache-manifest.4aa1cba163acd25aee0a01941924439c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f82ffa939975b495f9465c0b2ec291",
+    "url": "/countries/index.html"
+  },
+  {
+    "revision": "4f08ec167dede50df8ff",
+    "url": "/countries/static/css/main.2f9e86fe.chunk.css"
+  },
+  {
+    "revision": "66c2cf64127aec785859",
+    "url": "/countries/static/js/2.0dd2e46f.chunk.js"
+  },
+  {
+    "revision": "4f08ec167dede50df8ff",
+    "url": "/countries/static/js/main.cafd07d4.chunk.js"
+  },
+  {
+    "revision": "16f6e805d31332d2e90a",
+    "url": "/countries/static/js/runtime-main.c1d78f58.js"
+  }
+]);
